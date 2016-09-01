66d546b5fc328239fbef9e619d1fa6c91333a106
Mark Armendariz <src@enobrev.com>
2016-09-01 06:35:15 -0500

    attempting a split of function to file

--- src/Arrays.php
+++ src/Arrays.php
@@ -25,19 +25,3 @@
         }
         return false;
     }
-
-    /**
-     * @param string $sPath
-     * @param mixed $mValue
-     * @param string $sDelimiter
-     * @return array
-     */
-    function array_from_path($sPath, $mValue, $sDelimiter = '.') {
-        $aPath     = explode($sDelimiter, $sPath);
-        $aResponse = $mValue;
-        while(count($aPath) > 1) {
-            $aResponse = array(array_pop($aPath) => $aResponse);
-        }
-
-        return $aResponse;
-    }
