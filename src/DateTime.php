d55cbb36f03bbad73fc10280f909e73b1c4e9482
Mark Armendariz <src@enobrev.com>
2019-02-22 00:12:48 -0600

    Added minutes and minutes_ago date functions

--- src/DateTime.php
+++ src/DateTime.php
@@ -12,3 +12,35 @@
     function notNowButRightNow() {
         return DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
     }
+
+    function minutes(DateTime $oFrom, DateTime $oTo):int {
+        $oDiff  = $oFrom->diff($oTo);
+        $iDiff  = $oDiff->d * 24 * 60;
+        $iDiff += $oDiff->h * 60;
+        $iDiff += $oDiff->i;
+
+        if ($oDiff->invert) {
+            return -$iDiff;
+        }
+
+        return $iDiff;
+    }
+
+    function minutes_ago(DateTime $oFrom, DateTime $oTo) {
+        $oDiff  = $oFrom->diff($oTo);
+        $iDiff  = $oDiff->d * 24 * 60;
+        $iDiff += $oDiff->h * 60;
+        $iDiff += $oDiff->i;
+
+        $sMinutes = abs($iDiff) === 1 ? 'minute' : 'minutes';
+
+        if ($iDiff === 0) {
+            return 'now';
+        }
+
+        if ($oDiff->invert) {
+            return "$iDiff $sMinutes ago";
+        }
+
+        return "in $iDiff $sMinutes";
+    }
---
+++ tests/MinuteTest.php
@@ -0,0 +1,41 @@
+<?php
+    namespace Enobrev;
+
+    require __DIR__ . '/../vendor/autoload.php';
+
+    use DateTime;
+    use PHPUnit\Framework\TestCase;
+ 
+    class MinuteTest extends TestCase {
+        public function testMinutes() {
+            $oNow       = new DateTime();
+            $oThen      = new DateTime('-10 minutes');
+            $oLater     = new DateTime('+10 minutes');
+            $oThen1     = new DateTime('-2 minutes');
+            $oLater1    = new DateTime('+1 minutes');
+
+            $this->assertEquals(0,      minutes($oNow,     $oNow));
+            $this->assertEquals(-9,     minutes($oNow,     $oThen));
+            $this->assertEquals(10,     minutes($oNow,     $oLater));
+            $this->assertEquals(9,      minutes($oThen,    $oNow));
+            $this->assertEquals(-10,    minutes($oLater,   $oNow));
+            $this->assertEquals(-1,     minutes($oNow,     $oThen1));
+            $this->assertEquals(1,      minutes($oNow,     $oLater1));
+        }
+
+        public function testMinutesAgo() {
+            $oNow       = new DateTime();
+            $oThen      = new DateTime('-10 minutes');
+            $oLater     = new DateTime('+10 minutes');
+            $oThen1     = new DateTime('-2 minutes');
+            $oLater1    = new DateTime('+1 minutes');
+
+            $this->assertEquals('now',              minutes_ago($oNow,     $oNow));
+            $this->assertEquals('9 minutes ago',    minutes_ago($oNow,     $oThen));
+            $this->assertEquals('in 10 minutes',    minutes_ago($oNow,     $oLater));
+            $this->assertEquals('in 9 minutes',     minutes_ago($oThen,    $oNow));
+            $this->assertEquals('10 minutes ago',   minutes_ago($oLater,   $oNow));
+            $this->assertEquals('1 minute ago',     minutes_ago($oNow,     $oThen1));
+            $this->assertEquals('in 1 minute',      minutes_ago($oNow,     $oLater1));
+        }
+    }
